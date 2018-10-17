7766008a0a3fff07474eab279fc8bd92b85b7d48
BadiR3d <40484772+BadiR3d@users.noreply.github.com>
2018-10-17 09:17:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,31 +2,64 @@
 // where your node app starts
 
 // init project
+var path=require("path")
 var express = require('express');
 var app = express();
+const http = require('http');
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+var port = process.env.PORT || 8080;
+var monthNames = [ "January", "February", "March", "April", "May", "June", 
+                       "July", "August", "September", "October", "November", "December" ];
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.all('/:timestamp', function (request, response) {
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    var timeStamp = request.params.timestamp;
+    
+    var timeObj = {
+        "unix" : null,
+        "natural": null
+    };
+    
+    try {
+        
+        var naturalTime = timeStamp.toString().split(' ');
+        
+        if (naturalTime.length === 3) {
+            
+            var dt = new Date(timeStamp.toString());
+            timeObj["natural"] = timeStamp.toString(); 
+            timeObj["unix"] = dt.valueOf() / 1000;
+            //response.write('Natural Date!');
+
+        } else {
+            throw new Error('Not Natural Date!'); 
+        }
+
+    } catch (err1) {
+        console.log(err1);
+        try {
+            
+            var unixTime = Number(timeStamp);
+            
+            if (isNaN(unixTime))  { 
+                throw new Error('Not Unix Timestamp!');
+            } else {
+                timeObj["unix"] = unixTime;
+                
+                var dt = new Date(unixTime * 1000);
+                timeObj["natural"] = monthNames[dt.getMonth()] + " " + dt.getDate() + ", " + dt.getFullYear();
+            } 
+        } catch (err2) {
+            console.log(err2);
+        }
+    }
+
+    response.send(timeObj);
 });
 
+app.listen(port);
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.get('/',function(req,res) {
+    console.log(req.url);
+    res.sendFile(path.join(__dirname+'/index.html'));   
+})
